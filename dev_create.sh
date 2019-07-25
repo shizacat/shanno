ab3c3c7f1d19c62c48d67a248240003fa1fb4de5
Alexey Matveev <>
2019-07-25 12:33:13 +0300

    #FIX npm symlink

--- dev_create.sh
+++ dev_create.sh
@@ -13,7 +13,7 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt -y install yarn
 curl --silent --location https://deb.nodesource.com/setup_6.x | sudo bash -
-sudo apt install -y nodejs
+sudo apt install -y nodejs npm
 
 # ---
 echo "Install Gulp"
@@ -22,7 +22,7 @@ sudo npm install -g gulp
 # ---
 echo "Install Semantic UI"
 # pip install django2-semantic-ui
-npm install semantic-ui --save
+npm install semantic-ui --save --no-bin-link
 (
     cd semantic
     gulp build
@@ -30,6 +30,8 @@ npm install semantic-ui --save
 
 # ---
 echo "Install Python module"
+
+sudo apt install python3-pip python3-venv
 
 python3 -m venv ${VBASE}
 . ${VBASE}/bin/activate
