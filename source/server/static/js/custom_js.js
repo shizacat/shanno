302e1238128956305f6f024a3d934441ecbb822f
Dmitry Bogdanov <intokabil@gmail.com>
2019-07-27 14:10:53 +0300

    fix up js

--- source/server/static/js/custom_js.js
+++ source/server/static/js/custom_js.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-  $(".ui.sidebar").sidebar("attach events", ".toc.item");
   $('.ui .item').on('click', function() {
         $('.ui .item').removeClass('active');
         $(this).addClass('active');
