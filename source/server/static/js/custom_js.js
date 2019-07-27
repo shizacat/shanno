425d5a25272aa06c5ad20469ee1566058f569866
Dmitry Bogdanov <intokabil@gmail.com>
2019-07-27 14:09:26 +0300

    add new menu

--- source/server/static/js/custom_js.js
+++ source/server/static/js/custom_js.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-  $(".masthead").visibility({
-    once: false,
-    onBottomPassed: function() {
-      $(".fixed.menu").transition("fade in");
-    },
-    onBottomPassedReverse: function() {
-      $(".fixed.menu").transition("fade out");
-    }
-  });
   $(".ui.sidebar").sidebar("attach events", ".toc.item");
   $('.ui .item').on('click', function() {
         $('.ui .item').removeClass('active');
