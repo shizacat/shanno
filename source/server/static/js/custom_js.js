1de469626e526a2b97b1ccefee8c29334a9e2f98
Dmitry Bogdanov <intokabil@gmail.com>
2019-07-29 05:03:01 +0300

    add form

--- source/server/static/js/custom_js.js
+++ source/server/static/js/custom_js.js
@@ -1,6 +1,35 @@
-// $(document).ready(function() {
-//   $('.ui .item').on('click', function() {
-//         $('.ui .item').removeClass('active');
-//         $(this).addClass('active');
-//     });
-// });
+$(document).ready(function() {
+$('.ui.form')
+  .form({
+    on: 'blur',
+    fields: {
+      empty: {
+        identifier  : 'project-name',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a name'
+          }
+        ]
+      },
+      dropdown: {
+        identifier  : 'project-description',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter a description'
+          }
+        ]
+      },
+      checkbox: {
+        identifier  : 'file-input',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please upload a file'
+          }
+        ]
+      }
+    }
+  });
+});
