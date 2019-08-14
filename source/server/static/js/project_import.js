749abee8b1db7c39b9de5fc8cd313e1f10316d18
Alexey Matveev <>
2019-08-14 12:24:45 +0300

    Статус обработки файла

--- source/server/static/js/project_import.js
+++ source/server/static/js/project_import.js
@@ -1,11 +1,16 @@
 new Vue({
   el: "#project-import",
   data: {
-    files: '',
-    format: 'conllup'
+    files: null,
+    format: 'conllup',
+    st_variant: 'info', //success, danger
+    st_value: '---',
+    st_show: false,
   },
   methods: {
     uploadFiles: function(project_id) {
+      self = this;
+
       let fileData = new FormData;
       fileData.append("files", this.files);
       fileData.append("format", this.format);
@@ -17,15 +22,16 @@ new Vue({
           }
         }
       ).then(function(response) {
-        console.log(response);
+        self.st_variant = "success";
+        self.st_value = "Файл успешно обработан";
+        self.st_show = true;
+        self.files = null;
       })
       .catch(function(error) {
-          console.log("Получена ошибка");
-          console.log(error);
+        self.st_variant = "danger";
+        self.st_value = "Получена ошибка: " + error;
+        self.st_show = true;
       })
     },
-    handleFileUpload(){
-      this.files = this.$refs.files.files[0];
-     }
   }
 });
