69c249a4a9c06688f74c47afc0df20c88f9066de
Dmitry Bogdanov <intokabil@gmail.com>
2019-08-23 17:23:29 +0300

    pass annotation text

--- source/server/static/js/project_tl_annotation.js
+++ source/server/static/js/project_tl_annotation.js
@@ -1,5 +1,17 @@
 new Vue({
   el: "#project-annotaion",
+  delimiters: ['${', '}'],
+  data: {
+    doc_data: []
+  },
+  computed: {
+    doc_id: function(){
+      return window.location.href.split("?doc=")[1];
+    }
+  },
+    created() {
+    this.getDocSequence();
+  },
   methods: {
     onNext: function () {
       console.log("next");
@@ -8,6 +20,17 @@ new Vue({
       console.log(window.location.href);
       var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?doc=' + 10;
       window.history.pushState(null, null, newurl);
+    },
+    getDocSequence: function() {
+
+      axios.get("/api/document/" + this.doc_id + "/")
+      .then(response => {
+        this.doc_data = response.data.sequences
+        console.log(response)
+      })
+      .catch(function(error) {
+        console.log(error)
+      });
     }
-  },
+  }
 });
