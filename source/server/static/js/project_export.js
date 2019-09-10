1fd8ae9b45466be71aec4d53af6764ff23fd4c25
Alexey Matveev <>
2019-09-10 14:03:21 +0300

    work

--- source/server/static/js/project_export.js
+++ source/server/static/js/project_export.js
@@ -6,6 +6,7 @@ new Vue({
   methods: {
     download: function(project_id){
       self = this;
+      // window.location.href = "/api/project/" + project_id + "/ds_export/?exformat=" + this.format;
 
       axios({
         method: 'get',
@@ -16,7 +17,11 @@ new Vue({
         responseType: 'arraybuffer'
       })
       .then(function(response){
-        self.forceFileDownload(response);
+        const blob = new Blob([response.data])
+        let link = document.createElement('a')
+        link.href = window.URL.createObjectURL(blob)
+        link.download = 'export.zip'
+        link.click()
       })
       .catch(function(error){
         console.log("Error", error)
