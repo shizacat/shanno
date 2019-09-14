e83f540ce83677ed617a21a6a3d03db6650d8539
Alexey Matveev <>
2019-09-14 09:47:21 +0300

    Fix button export

--- source/server/static/js/project_export.js
+++ source/server/static/js/project_export.js
@@ -2,12 +2,13 @@ new Vue({
   el: "#project-export",
   data: {
     format: 'conllup',
+    is_process: false,
   },
   methods: {
     download: function(project_id){
       self = this;
-      // window.location.href = "/api/project/" + project_id + "/ds_export/?exformat=" + this.format;
 
+      this.is_process = true;
       axios({
         method: 'get',
         url: "/api/project/" + project_id + "/ds_export/",
@@ -17,14 +18,16 @@ new Vue({
         responseType: 'arraybuffer'
       })
       .then(function(response){
-        const blob = new Blob([response.data])
-        let link = document.createElement('a')
-        link.href = window.URL.createObjectURL(blob)
-        link.download = 'export.zip'
-        link.click()
+        const blob = new Blob([response.data]);
+        let link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'export.zip';
+        link.click();
+        self.is_process = false;
       })
       .catch(function(error){
-        console.log("Error", error)
+        self.is_process = false;
+        console.log("Error", error);
       });
     }
   }
