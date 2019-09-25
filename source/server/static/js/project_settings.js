04e334580097f0ca873b19ed2bafdbb0e0e537a1
Alexey Matveev <>
2019-09-25 11:57:35 +0300

    Error Alert - settings

--- source/server/static/js/project_settings.js
+++ source/server/static/js/project_settings.js
@@ -1,15 +1,33 @@
 new Vue({
   el: "#project-settings",
+  delimiters: ['${', '}'],
+  data: {
+    st_show: false,
+    st_variant: "danger",
+    st_value: "",
+  },
   methods: {
     deleteProject: function(project_id) {
       axios.delete("/api/project/" + project_id + "/")
         .then(function (response) {
             location.href = '/projects/';
         })
-        .catch(function (error) {
-          console.log("Получена ошибка");
-          console.log(error);
-        });
+        .catch(this.addErrorApi);
+    },
+    addError: function(msg){
+      this.st_variant = "danger";
+      this.st_value = msg;
+      this.st_show = true;
+    },
+    addErrorApi: function(error) {
+      let msg = "";
+      if (typeof error.response !== 'undefined'){
+        msg = "Ошибка: [" + error.response.status + "] ";
+        msg += JSON.stringify(error.response.data);
+      } else {
+        msg = "Ошибка: " + error.toString();
+      }
+      this.addError(msg);
     }
   }
 });
