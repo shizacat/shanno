5bf843e765e071444abdf5fe348486d1d55bd575
Alexey Matveev <>
2019-10-03 00:36:44 +0300

    Add into dev_create libpq-dev. Remove gulp and semantic.

--- dev_create.sh
+++ dev_create.sh
@@ -8,30 +8,18 @@ VBASE=venv
 sudo apt update
 
 # ---
-echo "Install NodeJS"
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt -y install yarn
-curl --silent --location https://deb.nodesource.com/setup_6.x | sudo bash -
-sudo apt install -y nodejs npm
-
-# ---
-echo "Install Gulp"
-sudo npm install -g gulp
-
-# ---
-echo "Install Semantic UI"
-# pip install django2-semantic-ui
-npm install semantic-ui --save --no-bin-link
-(
-    cd semantic
-    gulp build
-)
+# echo "Install NodeJS"
+# curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt-get update && sudo apt-get -y install yarn
+# curl --silent --location https://deb.nodesource.com/setup_6.x | sudo bash -
+# sudo apt-get install -y nodejs npm
 
 # ---
 echo "Install Python module"
 
-sudo apt install -y python3-pip python3-venv
+sudo apt-get install -y python3-pip python3-venv
+sudo apt-get install -y libpq-dev python3-dev build-essential
 
 python3 -m venv ${VBASE}
 . ${VBASE}/bin/activate
