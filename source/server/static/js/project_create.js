677de2c94465ec39753ae68e81067d5bbbec6b4f
Dmitry Bogdanov <intokabil@gmail.com>
2019-11-12 19:48:40 +0300

    add cancel button, redirect to page project

--- source/server/static/js/project_create.js
+++ source/server/static/js/project_create.js
@@ -1,49 +1,54 @@
+const router = new VueRouter({
+  routes: []
+});
+
 new Vue({
-    el: "#app-projects",
-    delimiters: ['${', '}'],
-    data: {
-      name: "",
-      description: null,
-      type: "",
-      st_show: false,
-      st_value: "",
-    },
-    methods: {
-      onSubmit: function(){
-        self = this;
-        axios.post("/api/project/",
-            JSON.stringify({
-                name: this.name,
-                description: this.description,
-                type: this.type
-            }),
-            {
-              headers: {
-                'Content-Type': 'application/json'
-              }
-            }
-          )
-          .then(function (response) {
-            self.name = "";
-            self.description = "";
-            self.type = "";
-            location.href = '/';
-          })
-          .catch(this.addErrorApi);
-      },
-      addError: function(msg){
-        this.st_value = msg;
-        this.st_show = true;
-      },
-      addErrorApi: function(error) {
-        let msg = "";
-        if (typeof error.response !== 'undefined'){
-          msg = "Ошибка: [" + error.response.status + "] ";
-          msg += JSON.stringify(error.response.data);
-        } else {
-          msg = "Ошибка: " + error.toString();
+  router,
+  el: "#app-projects",
+  delimiters: ['${', '}'],
+  data: {
+    name: "",
+    description: null,
+    type: "",
+    st_show: false,
+    st_value: "",
+  },
+  methods: {
+    onSubmit: function(){
+      self = this;
+      axios.post("/api/project/",
+        JSON.stringify({
+          name: this.name,
+          description: this.description,
+          type: this.type
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
         }
-        this.addError(msg);
-      }
+        )
+      .then(function (response) {
+        self.name = "";
+        self.description = "";
+        self.type = "";
+        location.href = '/projects/' + response.data.id;
+      })
+      .catch(this.addErrorApi);
+   },
+   addError: function(msg){
+    this.st_value = msg;
+    this.st_show = true;
+  },
+  addErrorApi: function(error) {
+    let msg = "";
+    if (typeof error.response !== 'undefined'){
+      msg = "Ошибка: [" + error.response.status + "] ";
+      msg += JSON.stringify(error.response.data);
+    } else {
+      msg = "Ошибка: " + error.toString();
     }
-  });
+    this.addError(msg);
+  }
+}
+});
